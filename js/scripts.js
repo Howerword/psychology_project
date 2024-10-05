6371553ee46983d4dae8037963527d2e4cae678f
Howerword <moroze776@gmail.com>
2024-10-05 10:11:54 +0300

    update

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
 document.addEventListener("partialLoaded", () => {});
+
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".navbar-links");
+
+hamburger.addEventListener("click", () => {
+	navLinks.classList.toggle("active");
+});
