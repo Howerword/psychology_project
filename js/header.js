c40a1d099e9361796d30b97e04b08084f64261b2
Howerword <moroze776@gmail.com>
2024-10-20 11:49:08 +0300

    ver 0.3.6

--- js/header.js
+++ js/header.js
@@ -8,34 +8,6 @@ window.addEventListener("scroll", () => {
 	}
 });
 
-// scroll to section
-const menuLinks = document.querySelectorAll(".menu-link[data-goto]");
-if (menuLinks.length > 0) {
-	menuLinks.forEach((menuLink) => {
-		menuLink.addEventListener("click", onMenuLinkClick);
-	});
-
-	function onMenuLinkClick(e) {
-		const menuLink = e.target;
-		if (
-			menuLink.dataset.goto &&
-			document.querySelector(menuLink.dataset.goto)
-		) {
-			const gotoBlock = document.querySelector(menuLink.dataset.goto);
-			const gotoBlockValue =
-				gotoBlock.getBoundingClientRect().top +
-				pageYOffset -
-				document.querySelector("header").offsetHeight;
-
-			window.scrollTo({
-				top: gotoBlockValue,
-				behavior: "smooth",
-			});
-			e.preventDefault();
-		}
-	}
-}
-
 // burger menu
 const menuIcon = document.querySelector(".menu-icon");
 const menuBody = document.querySelector(".menu-body");
