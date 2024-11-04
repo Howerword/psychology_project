2cdd49a621def288451937216a6d465ac5bdf63c
Howerword <moroze776@gmail.com>
2024-11-04 16:08:20 +0200

    add scroll

--- js/header.js
+++ js/header.js
@@ -19,3 +19,24 @@ if (menuIcon) {
 		menuBody.classList.toggle("active");
 	});
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+	const menuLinks = document.querySelectorAll(".menu-link");
+
+	if (menuLinks.length > 0) {
+		menuLinks.forEach((menuLink) => {
+			menuLink.addEventListener("click", function (e) {
+				e.preventDefault(); // Запобігаємо переходу за посиланням
+				const targetId = this.getAttribute("href").substring(1);
+				const targetElement = document.getElementById(targetId);
+
+				if (targetElement) {
+					window.scrollTo({
+						top: targetElement.offsetTop - 50, // Відняти висоту навбару (якщо фіксований)
+						behavior: "smooth",
+					});
+				}
+			});
+		});
+	}
+});
