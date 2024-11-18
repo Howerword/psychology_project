aeba59590afe06576f5db96d9f57679bc75acc32
Howerword <142175459+Howerword@users.noreply.github.com>
2024-11-18 21:08:17 +0200

    add active flip status to carousel cards

--- js/carousel.js
+++ js/carousel.js
@@ -1,5 +1,16 @@
+let activeCard = null;
+
 document.querySelectorAll('.card').forEach(card => {
 	card.addEventListener('click', () => {
-		card.classList.toggle('flipped');
+		if (activeCard && activeCard !== card) {
+			activeCard.classList.remove('flipped');
+		}
+		if (card.classList.contains('flipped')) {
+			card.classList.remove('flipped');
+			activeCard = null;
+		} else {
+			card.classList.add('flipped');
+			activeCard = card;
+		}
 	});
 });
