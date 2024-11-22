30ab73862570e499c630821797d9a2b95dd611d4
Howerword <142175459+Howerword@users.noreply.github.com>
2024-11-22 19:25:08 +0200

    change photo carousels

--- js/form.js
+++ js/form.js
@@ -1,11 +1,11 @@
 document
 	.getElementById("feedbackForm")
 	.addEventListener("submit", function (e) {
-		e.preventDefault(); // Зупиняємо стандартну поведінку відправлення форми
+		e.preventDefault(); 
 
 		const formData = new FormData(this);
 
-		fetch("https://formspree.io/f/mbljgodo", {
+		fetch("https://formspree.io/f/mgvezbzl", {
 			method: "POST",
 			body: formData,
 			headers: {
@@ -15,9 +15,9 @@ document
 			.then((response) => {
 				if (response.ok) {
 					alert(
-						"Повідомлення надіслано успішно! Дякуємо за зворотній зв'язок. Очікуйте відповіді від нашого спеціаліста."
+						"Повідомлення надіслано успішно! Очікуйте відповіді на Ваш запит."
 					);
-					document.getElementById("feedbackForm").reset(); // Очищаємо форму після успішної відправки
+					document.getElementById("feedbackForm").reset();
 				} else {
 					alert("Щось пішло не так. Спробуйте ще раз пізніше.");
 				}
