b24a87096bfd5d536f0e91731f851c96f04d1f8c
Howerword <142175459+Howerword@users.noreply.github.com>
2025-01-12 18:12:34 +0200

    fix header

--- js/header.js
+++ js/header.js
@@ -1,16 +1,18 @@
-// Додаємо слухача подій на скрол сторінки
+const menuIcon = document.querySelector(".menu-icon");
+const menuBody = document.querySelector(".menu-body");
+const menuLinks = document.querySelectorAll(".menu-link");
+const btnPrimary = document.querySelector(".btn-primary");
+
+
 window.addEventListener("scroll", () => {
 	const navbar = document.querySelector(".header");
 	if (window.scrollY > 50) {
-		navbar.classList.add("scrolled"); // Додаємо клас при прокручуванні вниз
+		navbar.classList.add("scrolled");
 	} else {
-		navbar.classList.remove("scrolled"); // Видаляємо клас, якщо сторінка повертається вверх
+		navbar.classList.remove("scrolled");
 	}
 });
 
-// burger menu
-const menuIcon = document.querySelector(".menu-icon");
-const menuBody = document.querySelector(".menu-body");
 
 if (menuIcon) {
 	menuIcon.addEventListener("click", () => {
@@ -20,23 +22,46 @@ if (menuIcon) {
 	});
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-	const menuLinks = document.querySelectorAll(".menu-link");
+function closeMenu() {
+	document.body.classList.toggle("lock", false);
+	menuIcon.classList.toggle("active", false);
+	menuBody.classList.toggle("active", false);
+}
 
-	if (menuLinks.length > 0) {
-		menuLinks.forEach((menuLink) => {
-			menuLink.addEventListener("click", function (e) {
-				e.preventDefault(); // Запобігаємо переходу за посиланням
-				const targetId = this.getAttribute("href").substring(1);
-				const targetElement = document.getElementById(targetId);
 
-				if (targetElement) {
-					window.scrollTo({
-						top: targetElement.offsetTop - 50, // Відняти висоту навбару (якщо фіксований)
-						behavior: "smooth",
-					});
-				}
-			});
+if (menuLinks.length > 0) {
+	menuLinks.forEach((menuLink) => {
+		menuLink.addEventListener("click", function (e) {
+			e.preventDefault();
+			const targetId = this.getAttribute("href").substring(1);
+			const targetElement = document.getElementById(targetId);
+
+			closeMenu();
+
+			if (targetElement) {
+				window.scrollTo({
+					top: targetElement.offsetTop - 50,
+					behavior: "smooth",
+				});
+			}
 		});
-	}
-});
+	});
+}
+
+
+if (btnPrimary) {
+	btnPrimary.addEventListener("click", function(e) {
+		e.preventDefault();
+		const targetId = this.getAttribute("href").substring(1);
+		const targetElement = document.getElementById(targetId);
+
+		closeMenu(); 
+
+		if (targetElement) {
+			window.scrollTo({
+				top: targetElement.offsetTop - 50,
+				behavior: "smooth",
+			});
+		}
+	});
+}
